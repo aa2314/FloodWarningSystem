00a67cb683f116a8506102ffa3774ce022401bc7
Yann Divet <yad22@cam.ac.uk>
2022-01-29 23:04:27 +0000

    IB complete

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,16 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from .utils import sorted_by_key
+from haversine import haversine
+
+
+def stations_by_distance(stations, p):
+    """Given a list of station objects and a coordinate p, 
+    returns a list of (station, distance) tuples, 
+    where distance (float) is the distance of the station (MonitoringStation) from the coordinate p."""
+    list_stations = []
+    for station in stations:
+        list_stations.append((station.name, haversine(station.coord, p)))
+    list_stations = sorted_by_key(list_stations, 1)
+    return list_stations
