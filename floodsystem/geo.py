7d1d3df10c0232eebb2f76023e1f8cf46a61403d
Bugra Ceylan <bc557@cam.ac.uk>
2022-02-06 01:51:45 +0000

    first attempt

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -19,3 +19,19 @@ def stations_by_distance(stations, p):
         list_stations.append((station.name, haversine(station.coord, p)))
     list_stations = sorted_by_key(list_stations, 1)
     return list_stations
+
+def stations_within_radius(stations, centre, r):
+    """ returns a list of all stations within radius r of a geographic coordinate x """
+    stations_within_R = []
+    for station in stations:
+        if haversine(station.coord, centre) <= r :
+            stations_within_R.append((station.name, haversine(station.coord, centre))) 
+    return sorted(stations_within_R)
+
+def rivers_with_station(stations):
+    rivers_with_st = []
+    for station in stations:
+        rivers_with_st.append((station.name, station.river))
+    return set(rivers_with_st)
+
+
