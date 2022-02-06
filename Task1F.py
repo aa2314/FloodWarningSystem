5421d502e897143df23142438985f76f7aa7dc50
Yann Divet <yad22@cam.ac.uk>
2022-02-06 16:12:49 +0000

    Task 1F complete

---
+++ Task1F.py
@@ -0,0 +1,12 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import inconsistent_typical_range_stations
+
+def run():
+    """Requirements for Task 1F"""
+    stations = build_station_list()
+    print(inconsistent_typical_range_stations(stations))
+    
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
