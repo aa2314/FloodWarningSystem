d6cb76b7c9f9437bc73c4c2e0d02dd22dc14f91e
Yann Divet <yad22@cam.ac.uk>
2022-02-23 19:35:30 +0000

    2B babyyyy

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -9,7 +9,7 @@ def stations_level_over_threshold(stations, tol):
     for station in stations:
         if station.relative_water_level() != None:
             if station.relative_water_level() >= tol:
-                liste.append((station, station.relative_water_level()))
+                liste.append((station.name, station.relative_water_level()))
     liste_finale = sorted_by_key(liste, 1, reverse=True)
     return liste_finale
 
