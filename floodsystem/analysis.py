28fafe56871e4fe50d3a2b837071d40f8b79a9f9
Bugra Ceylan <bc557@cam.ac.uk>
2022-02-28 10:40:31 +0000

    hey b

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib
 
 
-
+"""
 def polyfit(dates, levels, p):
-   
+    conv_dates = matplotlib.dates.date2num(dates)
     d0 = 1
-    shifted_dates = conv_dates[d0:]
+    shifted_dates = conv_dates - conv_dates[-d0]
     coefficients = np.polyfit(shifted_dates, levels, p)
     polynomial = np.poly1d(coefficients)
-    tpl = (polynomial, d0)
-    return tpl 
+    return polynomial, d0
+"""
+def polyfit(dates, levels, p):
+    x = matplotlib.dates.date2num(dates)
+    y = levels
+    p_coefficient = np.polyfit(x-x[0], y, p)
+    poly = np.poly1d(p_coefficient)
+    return poly, x[0]
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -15,10 +15,11 @@ def plot_water_levels(station, dates, levels):
     plt.tight_layout()
     return plt.show()
 
-def plot_water_level_with_fit(station, dates, levels, p):
-    plt.plot(dates, levels)
-    val = matplotlib.dates.date2num(dates)##
-    plt.plot(dates, polyfit[0](val))##
+"""def plot_water_level_with_fit(station, dates, levels, p):
+    plt.plot(dates, levels, label = "Actual Data")
+    f_val = matplotlib.dates.date2num(dates)
+    polynomial, d0 = polyfit(dates, levels, p)
+    plt.plot(dates, polynomial(f_val - f_val[-d0]))    
     plt.plot(dates, station.typical_range[0], label = "Typical Low")
     plt.plot(dates, station.typical_range[1], label = "Typical High")
     plt.xlabel("Date")
@@ -26,3 +27,17 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.title("Station: {}".format(station.name))
     plt.xticks(rotation=30)
     plt.tight_layout()
+    return plt.show()
+"""
+def plot_water_level_with_fit(station, dates, levels, p):
+    x = matplotlib.dates.date2num(dates)
+    y = levels
+    poly, d0 = polyfit(dates, levels, p)
+    plt.plot(dates, poly(x-d0))
+    plt.plot(dates, levels, label="Water Level")
+    plt.xlabel('date')
+    plt.ylabel('water level')
+    plt.xticks(rotation = 45)
+    plt.title(station.name)
+    plt.tight_layout()
+    plt.show()
