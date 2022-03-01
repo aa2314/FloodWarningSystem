a3f5fc1f8c53cf879bee933e2af8e65cb011984f
Yann Divet <yad22@cam.ac.uk>
2022-03-01 23:58:23 +0000

    tests 2B & 2C

--- test_station.py
+++ test_station.py
@@ -32,3 +32,25 @@ def test_inconsistent_typical_range_stations():
     stations = build_station_list()
     liste = floodsystem.station.inconsistent_typical_range_stations(stations)
     assert len(liste) == 28
+
+
+def test_relative_water_level():
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    
+    # Set a value for latest level
+    s.latest_level = -2.3
+    # Check if relative water level is what is expected
+    assert s.relative_water_level() == 0 ## latest value is the minimum hence relative water level should be 0
+
+    # Likewise, we can check if this will work for the maximum value of the typical range
+    s.latest_level = 3.4445
+    assert s.relative_water_level() == 1
+
